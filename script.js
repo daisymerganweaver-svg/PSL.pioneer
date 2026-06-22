987bd268cc340ec63438c4928f3d497bed547720
daisymerganweaver-svg <daisymerganweaver@gmail.com>
2026-06-22 19:47:36 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+console.log("PIONEER SERVICES LOAN (PSL)");
+
+});
+
+function successMessage(){
+alert("Votre demande a été envoyée avec succès.");
+}
